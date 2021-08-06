18e8ad92013269f489dab14604de9300fe5a0bbd
Pasha <Pashakhurtenko@gmail.com>
2021-08-06 13:18:58 +0300

    do chat with admins

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,7 +4,10 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Запустить бота"),
+            types.BotCommand("start", "Запустити бота"),
             types.BotCommand("help", "Вывести справку"),
+            types.BotCommand("support_call", "Поспілкуватись з техпідтримкою"),
+            types.BotCommand("support", "Написати в техпідтримку"),
+            
         ]
     )
